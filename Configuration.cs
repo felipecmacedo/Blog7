93cbd6f6730439fa2670d78bd6629c5bb7de3066
Felipe Cimardi Macedo <felipecimardimacedo97@gmail.com>
2023-01-07 20:34:48 -0300

    Update Program

---
+++ Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog7;
+
+public static class Configuration
+{
+    //TOKEN
+    public static string JwtKey { get; set; } = "ZjUgr8ZZmAiEeIGKRnMfM6082J1nBO34=";
+}
---
+++ Services/TokenServices.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Blog7.Services
+{
+    public class TokenServices
+    {
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+            var tokenDescriptor = new SecurityTokenDescriptor();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
